84aaf21c1dd9062e87420e194fa02205dee757d0
Rishav kumar <143308773+rishavi1@users.noreply.github.com>
2025-07-24 17:45:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
 const typed = new Typed('.multiple-text',{
-    strings:['Frontend developer', 'Canva Designer', 'Video Editor'],
+    strings:['App Developer', 'Web Developer', 'IT Technician'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
